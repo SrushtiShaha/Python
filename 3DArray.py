ceba1283c98392408a43dd492374f4b5edd8e437
Srushti Sudhir Shaha <95905998+SrushtiShaha@users.noreply.github.com>
2026-06-07 14:09:00 +0530

    Python

---
+++ 3DArray.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Create a 3D array
+arr = np.array([
+    [[1, 2, 3],
+     [4, 5, 6]],
+    
+    [[7, 8, 9],
+     [10, 11, 12]]
+])
+
+print("3D Array:\n", arr)
+print("\nShape of array:", arr.shape)
+print("Number of dimensions:", arr.ndim)
