78f9a5b01e463de513d2e19da24981317b4b447a
Srushti Sudhir Shaha <95905998+SrushtiShaha@users.noreply.github.com>
2026-06-22 12:01:04 +0530

    Python

---
+++ CrossProductTwoVectors.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Define two vectors
+vector1 = np.array([1, 2, 3])
+vector2 = np.array([4, 5, 6])
+
+# Compute cross product
+cross_product = np.cross(vector1, vector2)
+
+print("Vector 1:", vector1)
+print("Vector 2:", vector2)
+print("Cross Product:", cross_product)
