012cd5f85981729f6664dcc873c2fa203adafca5
Srushti Sudhir Shaha <95905998+SrushtiShaha@users.noreply.github.com>
2026-07-07 19:28:09 +0530

    Python

---
+++ MathematicalOperations.py
@@ -0,0 +1,16 @@
+import numpy as np
+a = np.array([10,20,30,40,50])
+b = np.array([1,2,3,4,6])
+
+#Arithmetic Operations
+print("Addition -> ", a+b)
+print("Subtraction -> ", a-b)
+print("Multiplication -> ", a*b)
+print("Division -> ", a/b)
+print("Modulus -> ", a%b)
+
+#Universal Operations
+print("Square root -> \n",np.sqrt(a))
+print("Exponential -> \n",np.exp(b))
+print("Logarithm -> \n",np.log(a))
+print("Power -> \n",np.power(b,2))
