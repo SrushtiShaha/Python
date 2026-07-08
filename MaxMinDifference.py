eaf9454fabe49e75ad69c6e945b89821d6513c71
Srushti Sudhir Shaha <95905998+SrushtiShaha@users.noreply.github.com>
2026-07-08 21:14:47 +0530

    Python

---
+++ MaxMinDifference.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Sample array
+arr = np.array([10, 20, 4, 45, 99])
+
+# Difference between max and min
+difference = np.max(arr) - np.min(arr)
+
+print("Array:", arr)
+print("Difference between max and min:", difference)
